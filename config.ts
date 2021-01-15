e29206aa55e35379755bdba818214255a69350b5
mthoolen <matthijs.thoolen@codesquad.nl>
2021-01-15 16:07:53 +0100

    copied and edited to fit personal purposes

---
+++ config.ts
@@ -0,0 +1,67 @@
+export const CONFIG = {
+  social: [
+    {
+      name: 'Github',
+      username: 'mthoolen',
+      color: '12100E',
+      url: 'https://github.com/mthoolen',
+    },
+    {
+      name: 'Twitter',
+      color: '1DA1F2',
+      url: 'https://twitter.com/MThoolen',
+    },
+    {
+      name: 'LinkedIn',
+      color: '0077B5',
+      url: 'https://www.linkedin.com/in/matthijs-thoolen-4757a812a/',
+    },
+    {
+      name: 'Goodreads',
+      color: '4285F4',
+      logo: 'google-chrome',
+      url: 'https://www.goodreads.com/review/list/99045566?ref=nav_mybooks',
+    },
+  ],
+  github: {
+    username: 'mthoolen',
+    colors: {
+      title: '24292e',
+      text: '24292e',
+      icon: '24292e',
+      background: 'ffffff',
+    },
+    stats: {
+      mostUsedLanguages: true,
+      overallStats: true,
+    },
+    highlightedRepos: ['mthoolen'],
+  },
+  badges: {
+    enabled: true,
+    spectrum: ['46a2f1', '311C87', 'DD0031', '43853d'],
+    list: [
+      {
+        name: 'Java',
+      },
+      {
+        name: 'Kotlin',
+      },
+      {
+	name: 'Haskell',
+      },
+      {
+	name: 'Clojure',
+      },
+      {
+	name: 'Groovy',
+      },
+      {
+	name: 'Angular',
+      },
+      {
+	name: 'Vue',
+      }
+    ],
+  },
+};
